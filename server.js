e66bd30693a157f750d9d6b835e3b0825feb158a
RolandBykof <rolandbykof@gmail.com>
2025-06-10 22:04:49 +0300

    serverin palautus
    epäilen, että viimeisin server estää pelinviennin. Palautin edellisen version ja tarkistan, oliko pelinvienti siinä mahdollista.

--- server.js
+++ server.js
@@ -2590,21 +2590,16 @@ function determineDeclarerAndDummy(table) {
   
   // Set declarer and dummy
   if (declarerPartnership && firstPlayer) {
-    // Solo game: Always make South declarer if NS wins
-    if (table.isSoloGame && declarerPartnership === 'north-south') {
-      table.biddingState.declarer = 'south';
-      table.biddingState.dummy = 'north';
-    } else {
-      table.biddingState.declarer = firstPlayer;
-      const dummyIndex = (partnerships[declarerPartnership].indexOf(firstPlayer) + 1) % 2;
-      table.biddingState.dummy = partnerships[declarerPartnership][dummyIndex];
-    }
+    table.biddingState.declarer = firstPlayer;
+    const dummyIndex = (partnerships[declarerPartnership].indexOf(firstPlayer) + 1) % 2;
+    table.biddingState.dummy = partnerships[declarerPartnership][dummyIndex];
   } else {
     // Fallback
     table.biddingState.declarer = 'south';
     table.biddingState.dummy = 'north';
   }
 }
+
 /**
  * Move to play phase
  * @param {Object} table - Table object
